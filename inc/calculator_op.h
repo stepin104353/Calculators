90416aee7d7884590d3eaa18246d49de3b552453
stepin104353 <69048783+stepin104353@users.noreply.github.com>
2020-09-28 00:14:28 +0530

    Add files via upload

---
+++ inc/calculator_op.h
@@ -0,0 +1,11 @@
+#ifndef __CALCULATOR_OPERATIONS_H__
+#define __CALCULATOR_OPERATIONS_H__
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+int add(int number1,int number2);
+int subtract(int number1,int number2);
+int multiply(int number1,int number2);
+int division(int number1,int number2);
+int modulus(int number1,int number2);
+#endif
